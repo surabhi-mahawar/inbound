742c990e9bf5131142fc17f23fa54a26a77f43f5
chaks03 <chakshu@samagragovernance.in>
2021-07-01 21:09:51 +0530

    Fix: Controller issues

--- src/main/java/com/uci/inbound/Inbound.java
+++ src/main/java/com/uci/inbound/Inbound.java
@@ -2,18 +2,24 @@ package com.uci.inbound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.cassandra.repository.config.EnableReactiveCassandraRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
 
-@SpringBootApplication
 @EnableKafka
 @EnableReactiveCassandraRepositories("com.uci.dao")
-@PropertySource("application-messagerosa.properties")
-@PropertySource("application.properties")
-@PropertySource("application-adapter.properties")
-@ComponentScan(basePackages = {"com.samagra.*", "com.uci.utils"})
+@EntityScan("com.uci.dao")
+@PropertySources({
+		@PropertySource("classpath:application-messagerosa.properties"),
+		@PropertySource("classpath:application.properties"),
+		@PropertySource("classpath:application-adapter.properties"),
+		@PropertySource("classpath:dao-application.properties"),
+})
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.uci.inbound", "com.uci.adapter", "com.uci.utils"})
 public class Inbound {
 	public static void main(String[] args) {
 		SpringApplication.run(Inbound.class, args);
--- src/main/java/com/uci/inbound/health/HealthController.java
+++ src/main/java/com/uci/inbound/health/HealthController.java
@@ -15,6 +15,7 @@ public class HealthController {
 
     @RequestMapping(value = "/health", method = RequestMethod.GET, produces = { "application/json", "text/json" })
     public ResponseEntity<JsonNode> statusCheck() throws JsonProcessingException {
+        System.out.println("Test Health Check");
         ObjectMapper mapper = new ObjectMapper();
         JsonNode json = mapper.readTree("{\"id\":\"api.content.health\",\"ver\":\"3.0\",\"ts\":\"2021-06-26T22:47:05Z+05:30\",\"params\":{\"resmsgid\":\"859fee0c-94d6-4a0d-b786-2025d763b78a\",\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"checks\":[{\"name\":\"redis cache\",\"healthy\":true},{\"name\":\"graph db\",\"healthy\":true},{\"name\":\"cassandra db\",\"healthy\":true}],\"healthy\":true}}");
         return ResponseEntity.ok(json);
