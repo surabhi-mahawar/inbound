c63bcb31a70d9c290afc34dfd888dc19f299b169
Surabhi <surabhi.mahawar@aurigait.com>
2021-07-29 14:16:14 +0530

    use common health service nodes

--- src/main/java/com/uci/inbound/health/HealthController.java
+++ src/main/java/com/uci/inbound/health/HealthController.java
@@ -3,22 +3,14 @@ package com.uci.inbound.health;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.uci.utils.BotService;
-import com.uci.utils.kafka.KafkaConfig;
+import com.uci.utils.HealthService;
 
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.cassandra.CassandraHealthIndicator;
-import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.data.cassandra.core.CassandraOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,72 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 	
-	@Value("${campaign.url}")
-	String campaignUrl;
-	
 	@Autowired
-	private KafkaConfig kafkaConfig;
-	
-	@Autowired
-	private CassandraOperations cassandraOperations;
-	
-	@Autowired
-	private BotService botService;
+	private HealthService healthService;
 	
     @RequestMapping(value = "/health", method = RequestMethod.GET, produces = { "application/json", "text/json" })
     public ResponseEntity<JsonNode> statusCheck() throws JsonProcessingException, IOException {
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode json = mapper.readTree("{\"id\":\"api.content.health\",\"ver\":\"3.0\",\"ts\":\"2021-06-26T22:47:05Z+05:30\",\"params\":{\"resmsgid\":\"859fee0c-94d6-4a0d-b786-2025d763b78a\",\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"checks\":[{\"name\":\"redis cache\",\"healthy\":true},{\"name\":\"graph db\",\"healthy\":true},{\"name\":\"cassandra db\",\"healthy\":true}],\"healthy\":true}}");
-        JsonNode resultNode = json.get("result");
+        JsonNode jsonNode = mapper.readTree("{\"id\":\"api.content.health\",\"ver\":\"3.0\",\"ts\":\"2021-06-26T22:47:05Z+05:30\",\"params\":{\"resmsgid\":\"859fee0c-94d6-4a0d-b786-2025d763b78a\",\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"checks\":[{\"name\":\"redis cache\",\"healthy\":true},{\"name\":\"graph db\",\"healthy\":true},{\"name\":\"cassandra db\",\"healthy\":true}],\"healthy\":true}}");
         
-        /* Cassandra health info */
-        CassandraHealthIndicator indicator = new CassandraHealthIndicator(cassandraOperations);
-    	String cassandraHealth = indicator.getHealth(false).getStatus().toString();
-        ObjectNode cassandraNode = mapper.createObjectNode();
-        cassandraNode.put("name", "cassandra db");
-        cassandraNode.put("healthy", cassandraHealth.equals("UP"));
+        ((ObjectNode) jsonNode).put("result", healthService.getAllHealthNode());
         
-        /* Kafka health info */
-        HealthIndicator kafkaIndicator = kafkaConfig.kafkaHealthIndicator();
-        Map<String, Object> kafkaDetails = kafkaIndicator.health().getDetails();
-        String kafkaHealth = kafkaIndicator.getHealth(false).getStatus().toString();
-        
-        ObjectNode kafkaNode = mapper.createObjectNode();
-        /* Kafka Details node */
-        ObjectNode kafkaDetailNode = mapper.createObjectNode();
-        for(Map.Entry<String, Object> entry : kafkaDetails.entrySet()){
-        	kafkaDetailNode.put(entry.getKey(), entry.getValue().toString());
-        };
-
-        kafkaNode.put("name", "kafka");
-        kafkaNode.put("healthy", kafkaHealth.equals("UP"));
-        if(kafkaHealth.equals("UP")) {
-        	kafkaNode.put("details", kafkaDetailNode);
-        }
-        
-        /* Campaign url health info */
-        Boolean campaignStatus = botService.statusUrlCheck(campaignUrl);
-        ObjectNode campaignNode = mapper.createObjectNode();
-        campaignNode.put("name", "campaign");
-        campaignNode.put("healthy", campaignStatus);
-        
-        // create `ArrayNode` object
-        ArrayNode arrayNode = mapper.createArrayNode();
-        
-        // add JSON users to array
-        arrayNode.addAll(Arrays.asList(cassandraNode, kafkaNode, campaignNode));
-        
-        ((ObjectNode) resultNode).putArray("checks").addAll(arrayNode);
-        
-        /* System overall health */
-        if(cassandraHealth.equals("UP") && kafkaHealth.equals("UP") && campaignStatus) {
-        	((ObjectNode) resultNode).put("healthy", true);
-        } else {
-        	((ObjectNode) resultNode).put("healthy", false);
-        }
-        
-        return ResponseEntity.ok(json);
+        return ResponseEntity.ok(jsonNode);
     }
-    
-    
 }
--- src/main/java/com/uci/inbound/health/ServiceStatusController.java
+++ src/main/java/com/uci/inbound/health/ServiceStatusController.java
@@ -1,10 +1,12 @@
 package com.uci.inbound.health;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.uci.utils.BotService;
+import com.uci.utils.HealthService;
 import com.uci.utils.kafka.KafkaConfig;
 
 import lombok.extern.slf4j.Slf4j;
@@ -27,69 +29,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/service")
 public class ServiceStatusController {
 	
-	@Value("${campaign.url}")
-	String campaignUrl;
-	
-	@Autowired
-	private CassandraOperations cassandraOperations;
-	
-	@Autowired
-	private KafkaConfig kafkaConfig;
-	
-	@Autowired
-	private BotService botService;
-	
-    @RequestMapping(value = "/health", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-    public ResponseEntity<JsonNode> statusCheck() throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode json = mapper.readTree("{\"id\":\"api.content.service.health\",\"ver\":\"3.0\",\"ts\":null,\"params\":{\"resmsgid\":null,\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"healthy\":true}}");
-        return ResponseEntity.ok(json);
-    }
+	@Autowired 
+	private HealthService healthService;
     
     @RequestMapping(value = "/health/cassandra", method = RequestMethod.GET, produces = { "application/json", "text/json" })
     public ResponseEntity<JsonNode> cassandraStatusCheck() throws JsonProcessingException {
-    	CassandraHealthIndicator indicator = new CassandraHealthIndicator(cassandraOperations);
-    	String cassandraHealth = indicator.getHealth(false).getStatus().toString();
-    	
-    	ObjectMapper mapper = new ObjectMapper();
-        JsonNode json = mapper.readTree("{\"id\":\"api.content.service.health\",\"ver\":\"3.0\",\"ts\":null,\"params\":{\"resmsgid\":null,\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"healthy\":false}}");
-        JsonNode resultNode = json.get("result");
-        ((ObjectNode) resultNode).put("healthy", cassandraHealth.equals("UP"));
+    	JsonNode jsonNode = getResponseJsonNode();
+    	((ObjectNode) jsonNode).put("result", healthService.getCassandraHealthNode());
         
-        return ResponseEntity.ok(json);
+        return ResponseEntity.ok(jsonNode);
     }
     
     @RequestMapping(value = "/health/kafka", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-    public ResponseEntity<JsonNode> kafkaStatusCheck() throws JsonProcessingException {
-    	HealthIndicator kafkaIndicator = kafkaConfig.kafkaHealthIndicator();
-        Map<String, Object> kafkaDetails = kafkaIndicator.health().getDetails();
-        String kafkaHealth = kafkaIndicator.getHealth(false).getStatus().toString();
-    	
-    	ObjectMapper mapper = new ObjectMapper();
-        JsonNode json = mapper.readTree("{\"id\":\"api.content.service.health\",\"ver\":\"3.0\",\"ts\":null,\"params\":{\"resmsgid\":null,\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"healthy\":false}}");
-        JsonNode resultNode = json.get("result");
-        /* Kafka Details node */
-        ObjectNode kafkaDetailNode = mapper.createObjectNode();
-        for(Map.Entry<String, Object> entry : kafkaDetails.entrySet()){
-        	kafkaDetailNode.put(entry.getKey(), entry.getValue().toString());
-        };
-        ((ObjectNode) resultNode).put("healthy", kafkaHealth.equals("UP"));
-        if(kafkaHealth.equals("UP")) {
-        	((ObjectNode) resultNode).put("details", kafkaDetailNode);
-        }
+    public ResponseEntity<JsonNode> kafkaStatusCheck() throws IOException, JsonProcessingException {
+    	JsonNode jsonNode = getResponseJsonNode();
+    	((ObjectNode) jsonNode).put("result", healthService.getKafkaHealthNode());
         
-        return ResponseEntity.ok(json);
+        return ResponseEntity.ok(jsonNode);
     }
     
     @RequestMapping(value = "/health/campaign", method = RequestMethod.GET, produces = { "application/json", "text/json" })
     public ResponseEntity<JsonNode> campaignUrlStatusCheck() throws JsonProcessingException, IOException {
-    	Boolean campaignStatus = botService.statusUrlCheck(campaignUrl);
-    	
-    	ObjectMapper mapper = new ObjectMapper();
-        JsonNode json = mapper.readTree("{\"id\":\"api.content.service.health\",\"ver\":\"3.0\",\"ts\":null,\"params\":{\"resmsgid\":null,\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"healthy\":false}}");
-        JsonNode resultNode = json.get("result");
-        ((ObjectNode) resultNode).put("healthy", campaignStatus);
+    	JsonNode jsonNode = getResponseJsonNode();
+        ((ObjectNode) jsonNode).put("result", healthService.getCampaignUrlHealthNode());
         
-        return ResponseEntity.ok(json);
+        return ResponseEntity.ok(jsonNode);
+    }
+    
+    /**
+     * Returns json node for service response
+     * 
+     * @return JsonNode
+     * @throws JsonMappingException
+     * @throws JsonProcessingException
+     */
+    private JsonNode getResponseJsonNode() throws JsonMappingException, JsonProcessingException {
+    	ObjectMapper mapper = new ObjectMapper();
+    	JsonNode jsonNode = mapper.readTree("{\"id\":\"api.content.service.health\",\"ver\":\"3.0\",\"ts\":null,\"params\":{\"resmsgid\":null,\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"healthy\":false}}");
+        return jsonNode;
     }
 }
