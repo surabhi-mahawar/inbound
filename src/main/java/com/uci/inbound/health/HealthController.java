ecf59a9424d6d605b45c7b3c80889417faebb645
Surabhi <surabhi.mahawar@aurigait.com>
2021-07-29 15:17:58 +0530

    health api timestamp

--- src/main/java/com/uci/inbound/health/HealthController.java
+++ src/main/java/com/uci/inbound/health/HealthController.java
@@ -9,6 +9,8 @@ import com.uci.utils.HealthService;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -26,8 +28,14 @@ public class HealthController {
     @RequestMapping(value = "/health", method = RequestMethod.GET, produces = { "application/json", "text/json" })
     public ResponseEntity<JsonNode> statusCheck() throws JsonProcessingException, IOException {
         ObjectMapper mapper = new ObjectMapper();
+        /* Current Date Time */
+        LocalDateTime localNow = LocalDateTime.now();
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        String dateString = fmt.format(localNow).toString();
+        
         JsonNode jsonNode = mapper.readTree("{\"id\":\"api.content.health\",\"ver\":\"3.0\",\"ts\":\"2021-06-26T22:47:05Z+05:30\",\"params\":{\"resmsgid\":\"859fee0c-94d6-4a0d-b786-2025d763b78a\",\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"checks\":[{\"name\":\"redis cache\",\"healthy\":true},{\"name\":\"graph db\",\"healthy\":true},{\"name\":\"cassandra db\",\"healthy\":true}],\"healthy\":true}}");
         
+        ((ObjectNode) jsonNode).put("ts", dateString);
         ((ObjectNode) jsonNode).put("result", healthService.getAllHealthNode());
         
         return ResponseEntity.ok(jsonNode);
