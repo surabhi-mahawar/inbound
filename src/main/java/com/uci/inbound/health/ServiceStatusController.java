51db2ef246d00dc95304a1d8df8122f88f733684
Surabhi <surabhi.mahawar@aurigait.com>
2021-09-13 15:45:21 +0530

    service status controller error resolved

--- src/main/java/com/uci/inbound/health/ServiceStatusController.java
+++ src/main/java/com/uci/inbound/health/ServiceStatusController.java
@@ -41,77 +41,6 @@ public class ServiceStatusController {
 
 	@Autowired
 	private HealthService healthService;
-
-<<<<<<< HEAD
-	@RequestMapping(value = "/health/cassandra", method = RequestMethod.GET, produces = { "application/json",
-			"text/json" })
-	public ResponseEntity<JsonNode> cassandraStatusCheck() throws IOException, JsonProcessingException {
-		JsonNode jsonNode = getResponseJsonNode();
-		((ObjectNode) jsonNode).put("result", healthService.getCassandraHealthNode());
-
-		return ResponseEntity.ok(jsonNode);
-	}
-
-	@RequestMapping(value = "/health/kafka", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-	public ResponseEntity<JsonNode> kafkaStatusCheck()
-			throws IOException, JsonProcessingException, InterruptedException {
-		JsonNode jsonNode = getResponseJsonNode();
-		((ObjectNode) jsonNode).put("result", healthService.getKafkaHealthNode());
-
-		return ResponseEntity.ok(jsonNode);
-	}
-
-	@RequestMapping(value = "/health/campaign", method = RequestMethod.GET, produces = { "application/json",
-			"text/json" })
-	public ResponseEntity<JsonNode> campaignUrlStatusCheck() throws JsonProcessingException, IOException {
-		JsonNode jsonNode = getResponseJsonNode();
-		((ObjectNode) jsonNode).put("result", healthService.getCampaignUrlHealthNode());
-
-		return ResponseEntity.ok(jsonNode);
-	}
-
-	private static final Logger logger = LogManager.getLogger(TelemetryLogger.class);
-
-	/*
-	 * Test with custom kafka appender & custom layout, 
-	 * telemetry object build internally via custom layout mentioned in xml by sent message
-	 */
-	@RequestMapping(value = "/test/logs", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-	public ResponseEntity<JsonNode> testKafkaLogAppender() throws JsonProcessingException, IOException {
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode jsonNode = mapper.readTree("{\"responseCode\":\"OK\"}");
-		
-		logger.fatal("Fatal Test Message 1");
-      
-		logger.info("Info Test Message 1");
-      
-		logger.error("Error Test Message 1");
-		
-		logger.warn("Warn Test Message 1");
-		
-		return ResponseEntity.ok(jsonNode);
-	}
-
-	/**
-	 * Returns json node for service response
-	 * 
-	 * @return JsonNode
-	 * @throws JsonMappingException
-	 * @throws JsonProcessingException
-	 */
-	private JsonNode getResponseJsonNode() throws JsonMappingException, JsonProcessingException {
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode jsonNode = mapper.readTree(
-				"{\"id\":\"api.content.service.health\",\"ver\":\"3.0\",\"ts\":null,\"params\":{\"resmsgid\":null,\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"healthy\":false}}");
-		return jsonNode;
-	}
-=======
-    @RequestMapping(value = "/health", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-    public ResponseEntity<JsonNode> statusCheck() throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode json = mapper.readTree("{\"id\":\"api.content.service.health\",\"ver\":\"3.0\",\"ts\":null,\"params\":{\"resmsgid\":null,\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"healthy\":true}}");
-        return ResponseEntity.ok(json);
-    }
     
     @RequestMapping(value = "/health/cassandra", method = RequestMethod.GET, produces = { "application/json", "text/json" })
     public ResponseEntity<JsonNode> cassandraStatusCheck() throws IOException, JsonProcessingException {
@@ -149,5 +78,4 @@ public class ServiceStatusController {
     	JsonNode jsonNode = mapper.readTree("{\"id\":\"api.content.service.health\",\"ver\":\"3.0\",\"ts\":null,\"params\":{\"resmsgid\":null,\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"healthy\":false}}");
         return jsonNode;
     }
->>>>>>> 1011337be25f6d13991e44083933a03c15717ef7
 }
