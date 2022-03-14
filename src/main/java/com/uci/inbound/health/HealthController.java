378444724c699380d3d3313504aa301247bef082
Surabhi <surabhi.mahawar@aurigait.com>
2022-03-14 11:19:25 +0530

    minor fix

--- src/main/java/com/uci/inbound/health/HealthController.java
+++ src/main/java/com/uci/inbound/health/HealthController.java
@@ -8,19 +8,30 @@ import com.microsoft.azure.storage.StorageException;
 import com.microsoft.azure.storage.blob.CloudBlobContainer;
 import com.microsoft.azure.storage.blob.CloudBlockBlob;
 import com.uci.dao.service.HealthService;
+import com.uci.utils.azure.AzureBlobService;
 import com.uci.utils.cdn.samagra.MinioClientService;
 
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Base64;
+import java.util.Base64.Encoder;
 import java.util.UUID;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.compress.utils.FileNameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +45,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.azure.storage.blob.*;
 import com.azure.storage.blob.models.BlobProperties;
+import com.azure.storage.blob.sas.BlobSasPermission;
+import com.azure.storage.blob.sas.BlobServiceSasSignatureValues;
 
 @Slf4j
 @RestController
@@ -43,7 +56,7 @@ public class HealthController {
 	private HealthService healthService;
 	
 	@Autowired
-	private MinioClientService minioClientService;
+	private AzureBlobService azureBlobService;
 	
     @RequestMapping(value = "/health", method = RequestMethod.GET, produces = { "application/json", "text/json" })
     public ResponseEntity<JsonNode> statusCheck() throws JsonProcessingException, IOException {
@@ -54,10 +67,6 @@ public class HealthController {
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
         String dateString = fmt.format(localNow).toString();
         
-        String signedUrl = minioClientService.getCdnSignedUrl("bot-audio.mp3");
-        log.info("signedUrl: "+signedUrl);
-        
-        
         JsonNode jsonNode = mapper.readTree("{\"id\":\"api.content.health\",\"ver\":\"3.0\",\"ts\":\"2021-06-26T22:47:05Z+05:30\",\"params\":{\"resmsgid\":\"859fee0c-94d6-4a0d-b786-2025d763b78a\",\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"checks\":[{\"name\":\"redis cache\",\"healthy\":true},{\"name\":\"graph db\",\"healthy\":true},{\"name\":\"cassandra db\",\"healthy\":true}],\"healthy\":true}}");
         
 //        ((ObjectNode) jsonNode).put("ts", dateString);
@@ -66,62 +75,13 @@ public class HealthController {
         return ResponseEntity.ok(jsonNode);
     }
     
-//    @Value("${blob.connection-string}")
-//    String connectionString;
-//
-//    @Value("${blob.container-name}")
-//    String containerName;
-//    
-//    
-//    @RequestMapping(value = "/test", method = RequestMethod.GET)
-//    public ResponseEntity<ByteArrayResource> test() {
-//    	BlobClientBuilder client = new BlobClientBuilder();
-//    	client
-//    		.endpoint("https://ucitest.blob.core.windows.net/uci-test-container")
-//    		.sasToken("sp=racwdl&st=2022-03-08T10:41:05Z&se=2023-01-31T18:41:05Z&spr=https&sv=2020-08-04&sr=c&sig=vYkR0e4nYLK9jQg%2FlpowpeuHzWnvYpaEcMyAe9LCIdQ%3D");
-//        
-//        String file = "testing-1.jpg";
-//        byte[] data = getFile(client, file);
-//        ByteArrayResource resource = new ByteArrayResource(data);
-//
-//        return ResponseEntity
-//                .ok()
-//                .contentLength(data.length)
-//                .header("Content-type", "application/octet-stream")
-//                .header("Content-disposition", "attachment; filename=\"" + file + "\"")
-//                .body(resource);
-//    }
-//    
-//    public byte[] getFile(BlobClientBuilder client, String name) {
-//        try {
-//            File temp = new File(name);
-//            BlobProperties properties = client.blobName(name).buildClient().downloadToFile(temp.getPath());
-//            byte[] content = Files.readAllBytes(Paths.get(temp.getPath()));
-//            temp.delete();
-//            return content;
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
+    @RequestMapping(value = "/image-signed-url", method = RequestMethod.GET)
+    public void test() {
+    	log.info(azureBlobService.getFileSignedUrl("testing-1.jpg"));
+    }
     
-//    public URI uploadPicture(MultipartFile multipartFile) {
-//        URI uri;
-//        String multipartName = multipartFile.getName().replaceAll("[\n|\r|\t]", "_");
-//        log.info("Profile image uploading, image name: {}", multipartName);
-//
-//        try {
-////        	BlobStorageService.
-//            String extension = FileNameUtils.getExtension(multipartFile.getOriginalFilename());
-//            String fileName = String.join(".", UUID.randomUUID().toString(), extension);
-//            CloudBlockBlob blob = cloudBlobContainer.getBlockBlobReference(fileName);
-//            blob.upload(multipartFile.getInputStream(), -1);
-//            uri = blob.getUri();
-//        } catch (URISyntaxException | StorageException | IOException e) {
-//            log.error("upload picture exception: "+e.getMessage());
-//            return null;
-//        }
-////        return Optional.ofNullable(uri).orElseThrow(UploadPictureFailedException::new);
-//        return uri;
-//    }
+    @RequestMapping(value = "/azure-container-sas", method = RequestMethod.GET)
+    public void generateAzureContainerSASToken() {
+    	log.info(azureBlobService.generateContainerSASToken());
+    }
 }
